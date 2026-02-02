e2675823a8ee4babf65fe0b2d488d63b97913f22
Nibedita Kalo (Contractor) <Nibedita.Kalo+qvc@Contractor.qurateretail.com>
2026-02-02 20:22:50 +0530

    feat(data): implement not allowing the player to move after the game is won

--- app/src/main/java/com/bnp/tictactoe/data/GameRepositoryImpl.kt
+++ app/src/main/java/com/bnp/tictactoe/data/GameRepositoryImpl.kt
@@ -13,7 +13,7 @@ class GameRepositoryImpl : GameRepository {
 
     private var state = GameState()
     override fun makeMove(position: Int): GameState {
-        if (state.board[position] != null) {
+        if (state.board[position] != null || state.winner != null) {
             return state
         }
         val newBoard = state.board.toMutableList()
@@ -25,11 +25,30 @@ class GameRepositoryImpl : GameRepository {
     }
 
     private fun updateState(newBoard: List<Player?>): GameState {
+        val (winner, winningLine) = checkWinner(newBoard)
         state = state.copy(
                 board = newBoard,
                 currentPlayer = if (state.currentPlayer == Player.X) Player.O else Player.X,
+                winner = winner,
+                winningLine = winningLine
         )
         return state
+    }
+
+    private fun checkWinner(board: List<Player?>): Pair<Player?, List<Int>?> {
+        val winningPositions = listOf(
+                listOf(0, 1, 2), listOf(3, 4, 5), listOf(6, 7, 8), //horizontal
+                listOf(0, 3, 6), listOf(1, 4, 7), listOf(2, 5, 8),  //Vertical
+                listOf(0, 4, 8), listOf(2, 4, 6) //Diagonal
+        )
+        return winningPositions.firstNotNullOfOrNull { positions ->
+            val (a, b, c) = positions
+            if (board[a] != null && board[a] == board[b] && board[a] == board[c]) {
+                Pair(board[a], positions)
+            } else {
+                null
+            }
+        } ?: Pair(null, null)
     }
 
     override fun resetGame(): GameState {
