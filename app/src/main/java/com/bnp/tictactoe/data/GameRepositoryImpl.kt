75330581e5098aad676573725c6110b55414375a
Nibedita Kalo (Contractor) <Nibedita.Kalo+qvc@Contractor.qurateretail.com>
2026-02-02 20:10:10 +0530

    feat(data): implement updates board with player move

--- app/src/main/java/com/bnp/tictactoe/data/GameRepositoryImpl.kt
+++ app/src/main/java/com/bnp/tictactoe/data/GameRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.bnp.tictactoe.data
 
 import com.bnp.tictactoe.domain.model.GameState
+import com.bnp.tictactoe.domain.model.Player
 import com.bnp.tictactoe.domain.repository.GameRepository
 
 
@@ -8,10 +9,24 @@ import com.bnp.tictactoe.domain.repository.GameRepository
  * @Author: 2026-DEV2-029;
  * @DateCreated: Monday, February 02, 2026
  */
-class GameRepositoryImpl: GameRepository {
+class GameRepositoryImpl : GameRepository {
 
+    private var state = GameState()
     override fun makeMove(position: Int): GameState {
-        TODO("Not yet implemented")
+        val newBoard = state.board.toMutableList()
+            .also {
+                it[position] = state.currentPlayer
+            }
+
+        return updateState(newBoard)
+    }
+
+    private fun updateState(newBoard: List<Player?>): GameState {
+        state = state.copy(
+                board = newBoard,
+                currentPlayer = if (state.currentPlayer == Player.X) Player.O else Player.X,
+        )
+        return state
     }
 
     override fun resetGame(): GameState {
